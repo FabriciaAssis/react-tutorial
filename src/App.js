f1b068d5fe2931de2544178643a810f055ff728d
Fabricia Assis <fabricia.assis@aluno.ifsp.edu.br>
2023-02-02 17:57:10 -0300

    add welcome word

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem vindo ao mundo React!
+          <h1>siga-me os bons</h1>
         </p>
         <a
           className="App-link"
