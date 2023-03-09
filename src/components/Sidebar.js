c4754190b86bbb2586a38e2ed25d1e10c1c0fef7
Fabricia Assis <fabricia.assis@aluno.ifsp.edu.br>
2023-03-09 17:33:19 -0300

    add img and svg

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box'
+import {GiTurtle} from 'react-icons/gi'
 
 const Sidebar = () => {
     return (
@@ -8,6 +9,11 @@ const Sidebar = () => {
             height: 500
         }}>
             <h2>Sidebar</h2>
+            <img style={{
+                width: 200,
+                heigh: 123.83
+            }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVb9RKRrlqpsD1POmoMqvVDCW8Ty7o3B4B8Q&usqp=CAU"></img>
         </Box>
     )
 }
